11e27d86bce82f122b0a1e22e626596fcb8883ca
jonathan <jonathan.970329@gmail.com>
2022-01-05 14:52:33 +0100

    responsiveness for welcome

--- src/components/about-me/about-me.tsx
+++ src/components/about-me/about-me.tsx
@@ -11,12 +11,12 @@ const AboutMe: React.FC = () => {
           <div className="text-section">
 						<h1>Caroline livscoach</h1>
             <p>
-              Funderar du på ditt egna värde. Har du svårt att sätta gränser?
+              Funderar du på ditt egna värde? Har du svårt att sätta gränser?
               Har du tappat fotfästet och känner dig vilsen? Har du haft en
               utmattning, utbrändhet eller har det hänt något annat som rubbat
-              livet. Har du gått länge och känner att du inte vet hur eller vart
+              livet? Har du gått länge och känner att du inte vet hur eller vart
               du ska börja. Längtar du efter att hitta glädje och trygghet i dig
-              själv och till livet. Jag har själv gått igenom många tuffa
+              själv och till livet.<br/><br/> Jag har själv gått igenom många tuffa
               motgångar i livet bl a utmattnings depression och cancerbehandling
               med allt vad det innebär. Jag är redo att möta dig som tar dig
               tillbaka från en utmattning, diagnostiserad cancer under

04194d458232a03500c428bb50c42281a56f6a25
jonathan <jonathan.970329@gmail.com>
2022-04-02 17:30:22 +0200

    fixed text-image check for ismobile

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -4,14 +4,27 @@ import ImgText from "../img-text/img-text";
 import profile_1 from "../../assets/img/about/profile_1.jpg";
 
 const About: React.FC = () => {
-  const [isMobile, setIsMobile] = React.useState<boolean>(isMobileInitial());
+  const [isMobile, setIsMobile] = React.useState<boolean>();
+  
+  React.useEffect(() => {
+    //*Eventlistener for size of screen
+    const listenToResize = () => {
+      if (window.innerWidth <= 1200) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+    };
+    window.addEventListener("resize", listenToResize);
+    return () => window.removeEventListener("resize", listenToResize);
+  }, []);
 
-  function isMobileInitial(): boolean {
-    if (window.innerWidth <= 1200) {
-      return true;
-    }
-    return false;
-  }
+  // function isMobileInitial(): boolean {
+  //   if (window.innerWidth <= 1200) {
+  //     return true;
+  //   }
+  //   return false;
+  // }
 
   const text =
     "Min stora förändring började för ca 13 år sedan då jag fick en utmattningsdepression. Jag var väldigt illa däran och fick bra hjälp från sjukvården men kände att den inte räckte hela vägen. Jag hade möjligheten att göra en storstädning som hölls av dåvarande Icoach. Genom det fick jag upptäcka vem jag faktiskt var och varför jag var den jag var. Vi gjorde en djupdykning bland dåliga minnen och rensa bort det jag bärt med mig som en jättestor ryggsäck under åren. Saker som inte jag behövde bära på, saker jag kunde lägga där de hörde hemma";

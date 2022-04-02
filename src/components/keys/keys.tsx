5b5f3bef3843c943eda23c7f8cbc89d983e9588a
jonathan <jonathan.970329@gmail.com>
2022-04-02 12:25:52 +0200

    fixed close btn keys

--- src/components/keys/keys.tsx
+++ src/components/keys/keys.tsx
@@ -4,110 +4,113 @@ import { keys } from "../../assets/content/keys";
 import { KeyValuePair } from "../../domain/model/KeyValuePair.model";
 
 const Keys: React.FC = () => {
-    const refs = useRef([
-        React.createRef<HTMLSpanElement>(),
-        React.createRef<HTMLSpanElement>(),
-        React.createRef<HTMLSpanElement>(),
-        React.createRef<HTMLSpanElement>(),
-        React.createRef<HTMLSpanElement>(),
-        React.createRef<HTMLSpanElement>(),
-    ]);
+  const refs = useRef([
+    React.createRef<HTMLSpanElement>(),
+    React.createRef<HTMLSpanElement>(),
+    React.createRef<HTMLSpanElement>(),
+    React.createRef<HTMLSpanElement>(),
+    React.createRef<HTMLSpanElement>(),
+    React.createRef<HTMLSpanElement>(),
+  ]);
 
-    const [activeKey, setActiveKey] = useState<KeyValuePair>(keys[0]);
+  const [activeKey, setActiveKey] = useState<KeyValuePair>(keys[0]);
 
-    useEffect(() => {
-        const observer = new IntersectionObserver((entries) => {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add("image-animation");
-                }
-            });
-        });
-        const imageElement = document.querySelector(".image-container");
-        if (imageElement) {
-            observer.observe(imageElement);
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("image-animation");
         }
-    }, []);
-    useEffect(() => {
-        document
-            .querySelector(".image-container")
-            ?.classList.add("image-animation");
-    }, [activeKey]);
+      });
+    });
+    const imageElement = document.querySelector(".image-container");
+    if (imageElement) {
+      observer.observe(imageElement);
+    }
+  }, []);
+  useEffect(() => {
+    document
+      .querySelector(".image-container")
+      ?.classList.add("image-animation");
+  }, [activeKey]);
 
-    const changeKey = (id: number): void => {
-        document
-            .querySelector(".image-container")
-            ?.classList.remove("image-animation");
-        refs.current[activeKey.key].current?.classList.remove("active");
-        refs.current[id].current?.classList.add("active");
-        setActiveKey(keys[id]);
-    };
+  const changeKey = (id: number): void => {
+    document
+      .querySelector(".image-container")
+      ?.classList.remove("image-animation");
+    refs.current[activeKey.key].current?.classList.remove("active");
+    refs.current[id].current?.classList.add("active");
+    setActiveKey(keys[id]);
+  };
 
-    const [slideUp, setSlideUp] = React.useState<boolean>(false);
+  const [slideUp, setSlideUp] = React.useState<boolean>(false);
 
-    const readMore = () => {
-        setSlideUp(!slideUp);
-    };
+  const readMore = () => {
+    setSlideUp(!slideUp);
+  };
 
-    return (
-        <section id="seven-keys-component" className="keys-component">
-            <div className="key">
-                <div className={`key-section`}>
-                    <div
-                        className={`${
-                            slideUp ? "slide-up" : "slide-down"
-                        } key-wrapper`}
-                    >
-                        <div
-                            onClick={() => readMore()}
-                            className={`${
-                                slideUp ? "fade-in" : "fade-out"
-                            } btn-close`}
-                        >
-                        </div>
-                        <div className="key-content">
-                            <h1 className="title dance-script-headline">
-                                {activeKey.value.title}
-                            </h1>
-
-                            <div
-                                className={`${
-                                    !slideUp ? "fade-in" : "fade-out"
-                                } btn-readmore`}
-                            >
-                                <span onClick={() => readMore()}>Läs mer</span>
-                            </div>
-                        </div>
-                        <div className="image-container">
-                            <img
-                                alt="keyImage"
-                                className={`image`}
-                                src={activeKey?.value.image}
-                            ></img>
-                        </div>
-                        <div className="key-list">
-                            <ul>
-                                {keys.map((value, index) => (
-                                    <li key={index}>
-                                        <span
-                                            ref={refs.current[index]}
-                                            id={`key${index}`}
-                                            onClick={() => changeKey(index)}
-                                            className="dot"
-                                        ></span>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    </div>
-                    <article className="key-description text-section">
-                        <h1>{activeKey.value.title}</h1>
-                        <p className="paragraph">{activeKey.value.description}</p>
-                    </article>
-                </div>
+  return (
+    <section id="seven-keys-component" className="keys-component">
+      <div className="key">
+        <div className={`key-section`}>
+          <div className={`${slideUp ? "slide-up" : "slide-down"} key-wrapper`}>
+            {/* <div className="cross btn-close" onClick={() => readMore()}>
+              <div className="cross-stick">
+							</div>
+            </div> */}
+            <div
+              className={`${slideUp ? "fade-in" : "fade-out"} btn-close`}
+              onClick={() => readMore()}
+            >
+              <div className="cross">
+                <div className="cross-stick"></div>
+              </div>
             </div>
-        </section>
-    );
+            {/* <div
+              onClick={() => readMore()}
+              className={`${slideUp ? "fade-in" : "fade-out"} btn-close`}
+            ></div> */}
+            <div className="key-content">
+              <h1 className="title dance-script-headline">
+                {activeKey.value.title}
+              </h1>
+
+              <div
+                className={`${!slideUp ? "fade-in" : "fade-out"} btn-readmore`}
+              >
+                <span onClick={() => readMore()}>Läs mer</span>
+              </div>
+            </div>
+            <div className="image-container">
+              <img
+                alt="keyImage"
+                className={`image`}
+                src={activeKey?.value.image}
+              ></img>
+            </div>
+            <div className="key-list">
+              <ul>
+                {keys.map((value, index) => (
+                  <li key={index}>
+                    <span
+                      ref={refs.current[index]}
+                      id={`key${index}`}
+                      onClick={() => changeKey(index)}
+                      className="dot"
+                    ></span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <article className="key-description text-section">
+            <h1>{activeKey.value.title}</h1>
+            <p className="paragraph">{activeKey.value.description}</p>
+          </article>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Keys;

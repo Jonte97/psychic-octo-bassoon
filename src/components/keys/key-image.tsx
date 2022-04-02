17c87e420913e4370df0ad4a74d35e7e24549d8b
jonathan <jonathan.970329@gmail.com>
2022-04-02 15:54:06 +0200

    fíxed image keys

--- src/components/keys/key-image.tsx
+++ src/components/keys/key-image.tsx
@@ -2,12 +2,20 @@ import React from "react";
 
 interface KeyImageProps {
   imagePath: string;
-	slideUp: boolean;
+  slideUp: boolean;
 }
 
 const KeyImage: React.FC<KeyImageProps> = (props: KeyImageProps) => {
-
-  return <img className={`key-image image-animation`} src={props.imagePath} alt="image" />;
+  return (
+    <React.Fragment>
+      <img
+        className={`key-image image-animation`}
+        src={props.imagePath}
+        alt="image"
+      />
+      
+    </React.Fragment>
+  );
 };
 
 export default KeyImage;
--- src/components/keys/keys.tsx
+++ src/components/keys/keys.tsx
@@ -16,33 +16,15 @@ const Keys: React.FC = () => {
 
   const [activeKey, setActiveKey] = useState<KeyValuePair>(keys[0]);
 
-  // useEffect(() => {
-  //   const observer = new IntersectionObserver((entries) => {
-  //     entries.forEach((entry) => {
-  //       if (entry.isIntersecting) {
-  //         entry.target.classList.add("image-animation");
-  //       }
-  //     });
-  //   });
-  //   const imageElement = document.querySelector(".image-container");
-  //   if (imageElement) {
-  //     observer.observe(imageElement);
-  //   }
-  // }, []);
-  // useEffect(() => {
-  //   // document
-  //   //   .querySelector(".image-container")
-  //   //   ?.classList.add("image-animation");
-  // }, [activeKey]);
+  useEffect(() => {
+    refs.current[0].current?.classList.add("active");
+  }, []);
 
-  // const changeKey = (id: number): void => {
-  //   document
-  //     .querySelector(".image-container")
-  //     ?.classList.remove("image-animation");
-  //   refs.current[activeKey.key].current?.classList.remove("active");
-  //   refs.current[id].current?.classList.add("active");
-  //   setActiveKey(keys[id]);
-  // };
+  const changeKey = (id: number): void => {
+    refs.current[activeKey.key].current?.classList.remove("active");
+    refs.current[id].current?.classList.add("active");
+    setActiveKey(keys[id]);
+  };
 
   const [slideUp, setSlideUp] = React.useState<boolean>(false);
 
@@ -55,6 +37,20 @@ const Keys: React.FC = () => {
       <div className={`${slideUp ? "slide-up" : "slide-down"} image-wrapper`}>
         <div className={`image-container `}>
           <KeyImage slideUp={slideUp} imagePath={activeKey.value.image} />
+        </div>
+        <div className="key-list">
+          <ul>
+            {keys.map((value, index) => (
+              <li key={index}>
+                <span
+                  ref={refs.current[index]}
+                  id={`key${index}`}
+                  onClick={() => changeKey(index)}
+                  className="dot"
+                ></span>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
       <div className="read-more-wrapper ">
